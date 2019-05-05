b0f64507434682730b9af264f14de3121ffd6c9f
kfu02 <kevin.fu842@gmail.com>
2019-05-04 21:19:38 -0400

    oops

--- nnet/self_play.py
+++ nnet/self_play.py
@@ -37,7 +37,7 @@ def run_training_episode(player, games=1000):
         print(games-i, games*0.01)
         if games-i <= math.ceil(games*0.01):
             print("saving game", i)
-            save_game_log(game_log, filename=str(int(time.time()))) #save last 1% of games
+            save_game_log(log, filename=str(int(time.time()))) #save last 1% of games
 
     #save games after episode
     save_training_examples(training_examples)
